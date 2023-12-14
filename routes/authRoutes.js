958df21d12e7500d7cbdc283f2f6125309285617
jsgiovani <=>
2023-12-14 07:07:15 -0500

    logout impl

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { google, login, register } from '../controllers/AuthController.js';
+import { google, login, logout, register } from '../controllers/AuthController.js';
 
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/google', google);
+router.get('/logout', logout);
 
 export default router;
--- utils/veriyUser.js
+++ utils/veriyUser.js
@@ -4,8 +4,6 @@ import { errorHandler } from "./error.js";
 export const verifyToken =  (req, res, next) =>{
 
     const token = req.headers.authorization;
-
-    
     if (!token) {
         return next(errorHandler(401, 'Unauthorized, no token'));
     }
